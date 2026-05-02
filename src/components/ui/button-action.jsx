6fe95db0d667bca74345a2f53c3954b08daded90
DavidQRy <davidgui393@gmail.com>
2026-05-02 15:54:43 -0500

    feat: boton de acciones

--- src/components/ui/button-action.jsx
+++ src/components/ui/button-action.jsx
@@ -1,6 +1,10 @@
 export const ButtonAction = ({ label, onClick }) => {
   return (
-    <button type="button" onClick={onClick}>
+    <button
+      type="button"
+      onClick={onClick}
+      className="rounded-lg bg-[#575BBE] text-amber-50 px-7 py-3 font-semibold cursor-pointer  hover:opacity-50 transition-opacity"
+    >
       {label}
     </button>
   )
