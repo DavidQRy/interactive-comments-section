0cfcc6cd7856c04996f62ecdccc00885e8695f08
DavidQRy <davidgui393@gmail.com>
2026-05-13 21:50:52 -0500

    feat: delete comments

--- src/views/comments.jsx
+++ src/views/comments.jsx
@@ -1,12 +1,20 @@
 import { AddComment } from '@components/add-comment'
 import { CommentCard } from '@components/comment-card'
+import { DeleteModal } from '@components/modal-delete'
 import { ReplyList } from '@components/reply-list'
 import data from '@data/data.json'
 import { useComments } from '@hooks/useComments'
 
 export const Comments = () => {
   const { currentUser } = data
-  const { comments, handleVote, handleAdd } = useComments()
+  const {
+    comments,
+    handleVote,
+    handleAdd,
+    confirmDelete,
+    setDeletingId,
+    deletingId,
+  } = useComments()
 
   return (
     <section className="max-w-3xl mx-auto py-8 px-4 bg-very-light-gray min-h-screen flex flex-col gap-4">
@@ -17,12 +25,14 @@ export const Comments = () => {
             currentUser={currentUser}
             onUpvote={() => handleVote(comment.id, 1)}
             onDownvote={() => handleVote(comment.id, -1)}
+            onDelete={() => setDeletingId(comment.id)}
           />
           {comment.replies.length > 0 && (
             <ReplyList
               replies={comment.replies}
               currentUser={currentUser}
               handleVote={handleVote}
+              onDeleteReply={(id) => setDeletingId(id)}
             />
           )}
         </div>
@@ -31,6 +41,12 @@ export const Comments = () => {
         currentUser={currentUser}
         onSend={(txt) => handleAdd(txt, currentUser)}
       />
+      {deletingId && (
+        <DeleteModal
+          onConfirm={() => confirmDelete(deletingId)}
+          onCancel={() => setDeletingId(null)}
+        />
+      )}
     </section>
   )
 }
